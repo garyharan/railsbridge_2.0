12bfe979480ca079c1a83e3dd8dbcfacb240cc25
Gary Haran <gary.haran@gmail.com>
2013-11-09 15:04:38 -0500

    moved comments about

--- cipher.rb
+++ cipher.rb
@@ -2,6 +2,15 @@ class Caesar
   def self.cipher(str)
     str.tr("A-Za-z", "N-ZA-Mn-za-m")
   end
+
+  def self.decipher(str)
+  end
 end
 
 puts Caesar.cipher(ARGV.first)
+
+# Gary = Tnel
+# Mike = Zvxr
+# We want to translate from this letter to the next
+# abcdefghijklmnopqrstuvwxyz
+# nopqrstuvwxyzabcdefghijklm
